d506dc358ea60e1ec86f6ed8c9c90472bc6e2a45
100525972 <100525972@alumnos.uc3m.es>
2026-09-01 16:47:48 +0200

    security: remove malware remnants and disable automatic VS Code tasks

--- remove_js_injection.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let js = fs.readFileSync('assets/js/patrocinadores.js', 'utf8');
-
-// Use a regex to match the if (isEnglish) { ... } else { ... } block
-const regex = /if \(isEnglish\) \{\s+const callToActionHTML = [\s\S]*?[^;]+;\s+container\.insertAdjacentHTML\('beforeend', callToActionHTML\);\s+\} else \{\s+const callToActionHTML = [\s\S]*?[^;]+;\s+container\.insertAdjacentHTML\('beforeend', callToActionHTML\);\s+\}/g;
-
-js = js.replace(regex, '');
-fs.writeFileSync('assets/js/patrocinadores.js', js, 'utf8');
